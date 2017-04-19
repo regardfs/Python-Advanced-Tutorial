94454e65f860f654dfb0eae59200394ede210013
fengshuo <88981382@qq.com>
2017-04-20 00:40:15 +0800

    Update for recursion in json/yaml  leaf path

--- Decorator_usage_demo/decorator_wraps.py
+++ Decorator_usage_demo/decorator_wraps.py
@@ -17,3 +17,34 @@ def fib(n):
     if n <=1:
         return 1
     return fib(n-2) + fib(n-1)
+
+# change dict/list/tuple to object
+class obj(object):
+    dd = []
+    def __init__(self, d):
+        for a, b in d.iteritems():
+            if isinstance(b, (list, tuple)):
+               setattr(self, a, [obj(x) if isinstance(x, dict) else x for x in b])
+            else:
+               setattr(self, a, obj(b) if isinstance(b, dict) else b)
+
+
+
+for serivce_name, service_attrs in service_default_conf_yaml['etcd']['services'][project][service].iteritems():
+    print serivce_name, service_attrs
+
+prefix  = "/%s/%s/%s" % ('services', 'aliyun', 'slb')
+dd = {}
+
+# print leaf path of a tree
+def tree_leaf_path(path_dict, prefix, service_attrs):
+    for key, value in service_attrs.iteritems():
+        if not isinstance(value, dict):
+            path_dict["%s/%s" % (prefix, key)] = value
+        else:
+            test("%s/%s" % (prefix, key), value)
+
+
+with open(service_default_conf, "r") as stream:
+    service_default_conf_yaml = yaml.safe_load(stream)
+
